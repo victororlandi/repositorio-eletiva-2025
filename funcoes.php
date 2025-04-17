88a71cae378f9237f12a8c3acb55eb5e8b23528d
victororlandi <victororlandi2016@gmail.com>
2025-04-17 10:40:24 -0300

    atualizacao funcoes

--- funcoes.php
+++ funcoes.php
@@ -4,4 +4,12 @@ function soma($num1,$num2){
     return $num1 + $num2;
 }
 
+function subtracao($num1,$num2){
+    return $num1 - $num2;
+}
+
+function raiz($num1){
+    return sqrt($num1);
+}
+
 ?>
